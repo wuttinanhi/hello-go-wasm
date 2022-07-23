f45d61d8134141236102a64aa1eeb46a6adf5de7
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2022-07-24 00:41:48 +0700

    add send http request

--- main.go
+++ main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
+func SendGetRequest(url string) {
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	text := string(body)
+	fmt.Println(text)
+}
 func main() {
 	fmt.Println("Hello, wasm")
+	SendGetRequest("README.md")
 }
